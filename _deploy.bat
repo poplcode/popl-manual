efdea7c63a8518b4df4aee9b57617a6baed389c8
poplcode <code@popl.ml>
2020-03-09 18:39:12 +0900

    Update docs

--- _deploy.bat
+++ _deploy.bat
@@ -1,8 +1,11 @@
 cd D:\_my\Dropbox\src\github\poplcode\popl-manual
+rd /s /q _book
+rd /s /q docs
+
 call gitbook install
 call gitbook build
 
-call xcopy _book\*.* D:\_my\Dropbox\src\github\poplcode\poplcode.github.io\ /e /h /k
+call xcopy _book\*.* D:\_my\Dropbox\src\github\poplcode\poplcode.github.io\ /e /h /k /exclude:_deploy.bat+.git /Y
 
 git clean -fx node_modules
 git clean -fx _book
